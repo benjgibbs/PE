33b7d8a9725b0ad5653d289f5335768259045b20
Ben Gibbs <benjgibbs@gmail.com>
2012-06-14 23:07:06 +0100

    Solved problem 73

--- src/utils/Fraction.java
+++ src/utils/Fraction.java
@@ -1,21 +1,18 @@
 package utils;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 
 public class Fraction implements Comparable<Fraction>{
-	public final int n;
-	public final int d;
-	public final BigDecimal f;
-	public Fraction(int n, int d){
-		int gcd = gcd(n,d);
+	public final long n;
+	public final long d;
+	public final double f;
+	public Fraction(long n, long d){
+		long gcd = gcd(n,d);
 		this.n = n/gcd;
 		this.d = d/gcd;
-		this.f = (new BigDecimal(n)).divide(new BigDecimal(d),100,RoundingMode.HALF_UP);
+		this.f = (double)n/d;
 	}
 	
-	int gcd(int a, int b){
+	long gcd(long a, long b){
 		if(b==0){
 			return a;
 		}
@@ -25,15 +22,18 @@ public class Fraction implements Comparable<Fraction>{
 	@Override public String toString() {
 		return String.format("%d/%d", n,d);
 	}
-	
 	@Override public int compareTo(Fraction o) {
-		return f.compareTo(o.f);
+		if(o.f == f) return 0;
+		if(o.f < f) return 1;
+		return -1;
 	}
-
 	@Override public boolean equals(Object obj) {
 		if(!(obj instanceof Fraction))
 			return false;
 		Fraction rhs = (Fraction)obj;
 		return rhs.n == n && rhs.d == d;
+	}
+	@Override public int hashCode() {
+		return (int)(n*17 ^ d);
 	}
 }
