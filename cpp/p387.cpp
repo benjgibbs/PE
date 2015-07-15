f616ad720870eece29032eae3421d3a589acfb08
Ben Gibbs <benjgibbs@gmail.com>
2015-07-15 21:30:06 +0100

    starting harshads

--- cpp/p387.cpp
+++ cpp/p387.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-const int N = 10000;
+const long N = 10000;
 long s[N+1];
 
 long isHarshad(long n) {
-  int sum = 0;
-  int m = n;
+  long sum = 0;
+  long m = n;
   while(m > 0) {
     sum += m %10;
     m = m/10;
@@ -30,17 +30,17 @@ bool isStrongHarshad(long n) {
   return s[h] == 0;
 }
 
-
 int main(int argc, char** argv) {
   s[0] = s[1] = 1;
-  for (int i = 2; i <= N; i++) {
+  for (long i = 2; i <= N; i++) {
     if (s[i] != 0) continue;
-    for(int j = 2*i; j <= N; j += i) {
+    for(long j = 2*i; j <= N; j += i) {
       s[j] = i;
     }
   }
-  int sum = 0; 
-  for(int i = 100; i < N; i++) {
+  cout << "Sieve built." << endl;
+  long sum = 0; 
+  for(long i = 10; i < N; i++) {
     if (s[i] == 0 && isStrongHarshad(i/10) && isTruncatable(i/10)) {
       cout << "Adding: " << i << endl;
       sum += i;
