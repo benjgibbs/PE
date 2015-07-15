62156faf8ab6dbd3b5f0c65d89a9cffd84ca9731
Ben Gibbs <benjgibbs@gmail.com>
2015-07-15 18:52:33 +0100

    starting harshads

--- cpp/p357.cpp
+++ cpp/p357.cpp
@@ -13,12 +13,11 @@ using namespace std;
 bool check(const Sieve& sieve, long toCheck) {
   if (toCheck > 1 && toCheck % 2 == 1) return false;
   if (!sieve.isPrime(toCheck + 1)) return false;
-
-  for (long d = 1; d <= toCheck/2; d++) {
-    if ( toCheck % d == 0 ) {
-      // is divisor
+  
+  for (long d = 1; d*d <= toCheck; d++) {
+    if (toCheck % d == 0) {
       long x = d + toCheck / d;
-      if ( ! sieve.isPrime(x)) {
+      if (!sieve.isPrime(x)) {
         return false;
       }
     }
@@ -26,39 +25,22 @@ bool check(const Sieve& sieve, long toCheck) {
   return true;
 }
 
-void runSolve() {
-  long upto = 100000000;
+int main() {
+  long N = 100000000;
   cout << "Building sieve..." << endl;
-  Sieve sieve(upto+1);
-  cout << "... done" << endl;
+  boost::timer st;
+  Sieve sieve(N+1);
+  cout << "... done (" << st.elapsed() << ")" << endl;
+ 
   long sum = 0;
-  boost::timer resetTimer, durationTimer;
-  int slice = 10000;
-  for (long i = 1; i <= upto; i++) {
-    if( i % slice == 0) {
-      long remain = upto - i;
-      double togo1 =  resetTimer.elapsed() * (remain/slice) / 60;
-      double togo2 =  ((durationTimer.elapsed()/i) * remain) / 60;
-      cout << i  << ": " << setprecision(3) << resetTimer.elapsed() << "/" << togo1 
-           << "|" << setprecision(3) << durationTimer.elapsed() << "/" << togo2 << endl;
-      resetTimer.restart();
-    }
+  
+  boost::timer t;
+  for (long i = 1; i <= N; i++) {
     if (check(sieve, i)) {
       sum += i;
     }
   }
-  std::cout << "\nSum is: " << sum << endl;
-}
-
-
-int main() {
-  runSolve();
-/*
-  long upto = 100;
-  Sieve sieve(upto);
-  for(int i = 1; i < upto; i++) {
-    cout << i << ": " << check(sieve, i) << endl;
-  }
-  */
+  cout << "Sum is: " << sum  
+       << " (" << st.elapsed() << "/" << t.elapsed() << ")" << endl;
 }
 
